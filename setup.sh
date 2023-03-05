7ea53183e7504210b1ef27f3cbaad4c23c7e8ca7
Neon <neon@wltcs.us>
2023-03-05 16:27:19 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,42 +33,41 @@ cat << "EOF"
  |  __/  |  _ <  | |_| |  /  \  | |  | | | |_| |  /  \ 
  |_|     |_| \_\  \___/  /_/\_\ |_|  |_|  \___/  /_/\_\
 EOF
- echo "-------------------------------------------------------"
- currentyear=`date +'%Y'`
- echo "Thank you for using Neon's Proxmox Setup Script"
- echo "©️ $currentyear Neon Development"
- echo ""
- echo ""
- dark_theme() {
-   read -p "Would you like to install a dark theme? y/n " yesnotheme
-   if [ $yesnotheme == "y" ]
-       then
-       echo "Installing Weilbyte's PVE Discord Dark theme..."
-       apt install curl -y
-       bash <(curl https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh)
-       remove_popup()
-   fi
-   if [ $yesnotheme == "n" ]
-      then 
+echo "-------------------------------------------------------"
+currentyear=`date +'%Y'`
+echo "Thank you for using Neon's Proxmox Setup Script"
+echo "©️ $currentyear Neon Development"
+echo ""
+echo ""
+dark_theme() {
+  read -p "Would you like to install a dark theme? y/n " yesnotheme
+  if [ $yesnotheme == "y" ]
+      then
+      echo "Installing Weilbyte's PVE Discord Dark theme..."
+      apt install curl -y
+      bash <(curl https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh)
       remove_popup()
-    fi
- }
- remove_popup() {
-   read -p "Would you like to remove the Proxmox Subscription Notice message? y/n " yesnopopup
-   echo
-   if [ $yesnopopup== "y" ]
-       then
-       echo "Removing the subscription notice popup..."
-       sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
-       bedone()
+  fi
+  if [ $yesnotheme == "n" ]
+     then 
+     remove_popup()
    fi
-   if [ $yesnopopup == "n" ]
-      then 
+}
+remove_popup() {
+  read -p "Would you like to remove the Proxmox Subscription Notice message? y/n " yesnopopup
+  if [ $yesnopopup== "y" ]
+      then
+      echo "Removing the subscription notice popup..."
+      sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
       bedone()
-    fi 
- }
- bedone() {
- echo "Thank you for using Neon's Proxmox setup script! If you have any issues, please send them via the issues page on our Github located at https://github.com/WeLikeToCodeStuff/proxmox-setup/issues"
- exit 0
- }
- dark_theme()
+  fi
+  if [ $yesnopopup == "n" ]
+     then 
+     bedone()
+   fi 
+}
+bedone() {
+  echo "Thank you for using Neon's Proxmox setup script! If you have any issues, please send them via the issues page on our Github located at https://github.com/WeLikeToCodeStuff/proxmox-setup/issues"
+  exit 0
+}
+dark_theme()
